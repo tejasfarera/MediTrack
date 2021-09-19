e0fc2216fc9658f7c3cc4c8969012048f9d3e85f
macmini45 <mac@Chayan.local>
2021-09-19 21:09:43 +0530

    PROJECT: Project initialisation

---
+++ MediTrack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediTrack
+//
+//  Created by macmini45 on 19/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
