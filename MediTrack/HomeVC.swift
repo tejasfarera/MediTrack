92752d81be36a632bdab9a8641b63f52df37e52a
tejasfarera <tejas.farera@gmail.com>
2021-09-19 22:04:27 +0530

    HOME: UI height fixes for smaller devices and added reference

--- MediTrack/HomeVC.swift
+++ MediTrack/HomeVC.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class HomeVC: UIViewController {
+    
+    @IBOutlet weak var wishingLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var medicineButton: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        /// Currently just added the corner radius logic here as the button is only used once in project,
+        /// if there mutliple used, then create a subclass of UIButton with customisations
+        medicineButton.layer.cornerRadius = 15.0
     }
 
 
+    @IBAction func onPressMedicineTaken(_ sender: UIButton) {
+        
+    }
 }
 
