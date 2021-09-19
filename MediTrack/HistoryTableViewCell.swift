52987bbece9558a15e4c521ad22061bb24ee7341
tejasfarera <tejas.farera@gmail.com>
2021-09-20 01:45:31 +0530

    HISTORY_TABLE_CELL: Designed cell for history table

---
+++ MediTrack/HistoryTableViewCell.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+    
+    static let identifier = "HistoryTableViewCell"
+    
+    struct DataSource {
+        let afternoonTime: String
+        let morningTime: String
+        let eveningTime: String
+        let date: String
+        let score: Int
+    }
+    
+    @IBOutlet weak var morningStackView: UIStackView!
+    @IBOutlet weak var afternoonStackView: UIStackView!
+    @IBOutlet weak var eveningStackView: UIStackView!
+    
+    @IBOutlet weak var morningTimeLabel: UILabel!
+    @IBOutlet weak var afternoonTimeLabel: UILabel!
+    @IBOutlet weak var eveningTimeLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- MediTrack/HistoryVC.swift
+++ MediTrack/HistoryVC.swift
@@ -1,31 +1,41 @@
-//
-//  HistoryVC.swift
-//  MediTrack
-//
-//  Created by macmini45 on 19/09/21.
-//
-
 import UIKit
 
 class HistoryVC: UIViewController {
 
+    @IBOutlet private weak var historyTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.title = StoryBoardName.History.rawValue
 
-        // Do any additional setup after loading the view.
+        self.historyTableView.delegate = self
+        self.historyTableView.dataSource = self
+        
+        self.historyTableView.register(
+            UINib(nibName: HistoryTableViewCell.identifier, bundle: nil),
+            forCellReuseIdentifier: HistoryTableViewCell.identifier
+        )
+    }
+}
+
+extension HistoryVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HistoryTableViewCell.identifier, for: indexPath) as? HistoryTableViewCell else {
+            debugPrint("Can't type cast HistoryTableViewCell")
+            return HistoryTableViewCell()
+        }
+        
+        return cell
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
 }
