b392e06a21762b5749f8523586a06d195fa518a6
tejasfarera <tejas.farera@gmail.com>
2021-10-03 07:17:43 +0530

    DATE_EXTENSION: Added time format

--- MediTrack/Extension/Date+Extension.swift
+++ MediTrack/Extension/Date+Extension.swift
@@ -4,7 +4,14 @@ extension Date {
     
     var mmddyyy: String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd/mm/yyyy"
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        return dateFormatter.string(from: self)
+    }
+    
+    var time: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "hh:mm a"
         dateFormatter.locale = Locale(identifier: "en_US_POSIX")
         return dateFormatter.string(from: self)
     }
